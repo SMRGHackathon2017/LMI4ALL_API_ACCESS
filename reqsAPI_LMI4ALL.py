048800ea5bfd28561e0b21b71a9602752f556d37
SMRGHackathon2017 <GSRHackathon2017@gmail.com>
2017-07-07 12:51:44 +0100

    Update reqsAPI_LMI4ALL.py

--- reqsAPI_LMI4ALL.py
+++ reqsAPI_LMI4ALL.py
@@ -9,7 +9,9 @@ print (r.text) # check what we got. its everything together so a bit messy!
 json_r = r.json()    #parse the text into json
 
 
-#try to flatten json into csv. This is an initial try.
+#try to flatten json into csv. This is an initial try. 
+#if its really nested (date info in this case) 
+#more preprocessing needed here.
 
 
 df=pd.DataFrame.from_records(json_r)
